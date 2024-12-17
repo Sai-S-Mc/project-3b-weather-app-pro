e57e7bfdb8386929cc20de6ff144a601dfd085b2
Sai-S-Mc <sai.satti97@gmail.com>
2024-12-16 21:25:53 -0500

    Updated html, css and js files
    In html, removed temporary data to be replaced by real-time data requested from the API
    In js, injected more weather details into the html file using the API

--- src/script.js
+++ src/script.js
@@ -1,12 +1,24 @@
 function updateWeather(response) {
+  console.log(response.data);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
-
   let countryElement = document.querySelector("#country");
-  countryElement.innerHTML = response.data.country;
-
   let temperatureElement = document.querySelector("#current-temperature");
+  let descriptionElement = document.querySelector("#weather-description");
+  let realFeelElement = document.querySelector("#real-feel-temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#windspeed");
+
+  cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  realFeelElement.innerHTML = `feels like : ${Math.round(
+    response.data.temperature.feels_like
+  )}°C`;
+  humidityElement.innerHTML = `humidity : ${response.data.temperature.humidity}%`;
+  windspeedElement.innerHTML = `windspeed : ${Math.round(
+    response.data.wind.speed
+  )} km/h`;
 }
 function apiCitySearch(city) {
   let apiKey = "tbfob32e017e01391b34fe15b81ad2a6";
